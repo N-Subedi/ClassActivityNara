8f0e4185a796fa4483955f99d51bc417851a996a
N-Subedi <168014693+N-Subedi@users.noreply.github.com>
2024-10-14 17:06:42 -0400

    Update application.py
    Messi is the goat

--- application.py
+++ application.py
@@ -3,4 +3,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "<html><body><h1>Hello, Narayan</h1></body></html>\n"
+    return """
+    <html>
+    <body>
+        <h1>Hello, Narayan</h1>
+        <p>The greatest player of all time (GOAT) is Lionel Messi</p>
+        <img src="https://media.newyorker.com/photos/63a0b8e018f0244bba9fd613/master/pass/Mochkofsky-Messi.jpg" alt="Lionel Messi" width="500">
+    </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    app.run(debug=True)
